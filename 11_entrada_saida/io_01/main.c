95f450aeb41bcf1db3948f8efa32b71f68551606
Ricardo Taveira <ridtaveira@gmail.com>
2022-11-16 19:56:38 -0300

    Alterado em 16-11-2022

--- 11_entrada_saida/io_01/main.c
+++ 11_entrada_saida/io_01/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+
+void mostrar_Tela01();
+void mostrar_Tela02();
+void mostrar_Tela03();
+
+void mostrar_Tela01() {
+    char opcao;
+    do {
+        system("clear");
+        // Mostrar menu
+        printf("Cadastro de Alunos\n");
+        printf("------------------\n");
+        printf("1-Inserir Aluno\n");
+        printf("2-Mostrar Lista\n");
+        printf("3-Fim\n");
+        printf("Entre opcao-> ");
+        opcao = getchar();
+        if (opcao == '1')
+            mostrar_Tela02();
+        else if (opcao == '2')
+            mostrar_Tela03();
+    } while (opcao != '3');
+}
+
+void mostrar_Tela02(){
+    char opcao;
+    system("clear");
+    printf("\nTela_02");
+    opcao=getchar();
+}
+
+void mostrar_Tela03(){
+    char opcao;
+    system("clear");
+    printf("\nTela_03");
+    opcao=getchar();
+}
 
 int main() {
-    printf("Hello, World!\n");
+    mostrar_Tela01();
     return 0;
 }
