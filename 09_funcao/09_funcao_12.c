dd13a667677ab07982353fab1407841564b7aae7
Ricardo Taveira <ridtaveira@gmail.com>
2023-05-15 19:03:21 -0300

    Alterado em 15-05-2023

--- 09_funcao/09_funcao_12.c
+++ 09_funcao/09_funcao_12.c
@@ -1,20 +1,29 @@
 //
 //
 //
-
 #include <stdio.h>
 
-void print_matrix(int (*matrix)[3], int rows) {
+void print_matriz(int (*matrix)[3], int rows) {
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < 3; j++) {
-            printf("matrix[%d][%d]=%d ", i,j, matrix[i][j]);
+            printf("matriz[%d][%d]=%d ", i,j, matrix[i][j]);
         }
         printf("\n");
     }
 }
 
+void zera_matriz(int (*matrix)[3], int rows) {
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < 3; j++) {
+            matrix[i][j]=0;
+        }
+    }
+}
 int main() {
-    int matrix[2][3] = {{1, 2, 3}, {4, 5, 6}};
-    print_matrix(matrix, 2);
+    int matriz[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    print_matriz(matriz, 2);
+    printf("\n\n");
+    zera_matriz(matriz, 2);  
+    print_matriz(matriz, 2); 
     return 0;
 }
